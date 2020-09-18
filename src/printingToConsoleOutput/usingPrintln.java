cea12293e27e6118f8f6449be82a70f350c1c649
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2020-09-18 19:45:01 +0530

    printing details in single line using println statement.

---
+++ src/printingToConsoleOutput/usingPrintln.java
@@ -0,0 +1,18 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kumar Sanskar(KumarSanskar)
+ *   Date: 18-09-2020
+ *   Time: 07:38 PM
+ *   File: usingPrintln.java
+ */
+
+package printingToConsoleOutput;
+
+public class usingPrintln {
+    public static void main(String[] args) {
+        int ageOfPatient = 45;
+        String nameOfPatient = "Arun";
+        char rhFactor = 'A';
+
+        System.out.println("Age of Patient is: " + ageOfPatient + " Name Of Patient is: " + nameOfPatient + " rh Factor of Patient is:" + rhFactor);
+    }
+}
