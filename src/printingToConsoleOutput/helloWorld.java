10705d5c26f59dc4bfe8c73c8c8dc6ae16e59ecf
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2020-09-18 19:28:25 +0530

    Print Hello World to screen.

---
+++ src/printingToConsoleOutput/helloWorld.java
@@ -0,0 +1,14 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kumar Sanskar(KumarSanskar)
+ *   Date: 18-09-2020
+ *   Time: 07:25 PM
+ *   File: helloWorld.java
+ */
+
+package printingToConsoleOutput;
+
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
