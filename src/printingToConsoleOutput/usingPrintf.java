7ab8347f767602a6c1094d1e78f2f2a5a87cdda2
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2020-09-20 02:38:11 +0530

    Using printf to print values.

---
+++ src/printingToConsoleOutput/usingPrintf.java
@@ -0,0 +1,28 @@
+package printingToConsoleOutput;
+
+public class usingPrintf {
+    public static void main(String[] args) {
+
+        /*here we declare two variables studentName in String data-type and presentAge in int .
+         *
+         *       used for => format specifier
+         *
+         *       int, short, byte, long => %d
+         *       float, double => %f
+         *       char => %c
+         *       String => %s
+         *       hashcode => %h
+         *       newline or form feed => \n
+         *       tab => \t
+         *       backspace => \b
+         *       backlash => \\
+         *       carriage return => \r
+         *       line feed => \f
+         */
+        String studentName = "Ram";
+        int presentAge = 12;
+
+        System.out.printf("The name is %s and his age is %d.", studentName, presentAge);
+
+    }
+}
