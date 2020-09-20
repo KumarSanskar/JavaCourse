6a23ec514bdcac70a2e9d05c72d1e037a358c220
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2020-09-20 19:26:16 +0530

    Program showing the use of If block in a program.

---
+++ src/programmingConstructs/usingIfStatement.java
@@ -0,0 +1,13 @@
+package programmingConstructs;
+
+public class usingIfStatement {
+    public static void main(String[] args) {
+        // here we demonstrate using the if statement in a program.
+
+        int numberOfStudents = 69;
+        if (numberOfStudents == 69) {
+            System.out.println("There are sixty nine students in the class.");
+        }
+        System.out.println("This statement is general statement and is independent of if block .");
+    }
+}
