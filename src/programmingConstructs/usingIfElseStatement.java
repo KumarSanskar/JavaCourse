9dc54cbc709587717221bc5b59eb61b6a5528acc
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2020-09-22 01:45:20 +0530

    Program showing the use of If-else block in a program.

---
+++ src/programmingConstructs/usingIfElseStatement.java
@@ -0,0 +1,19 @@
+package programmingConstructs;
+
+import java.util.Scanner;
+
+public class usingIfElseStatement {
+    /*
+     * here we make a simple program which checks for the number to be even or odd using if else block.
+     */
+    public static void main(String[] args) {
+        System.out.println("Enter a number: ");
+        Scanner scannerObject = new Scanner(System.in);
+        int checkNumber = scannerObject.nextInt();
+        if (checkNumber % 2 == 0) {
+            System.out.println("Number is even.");
+        } else {
+            System.out.println("Number is odd.");
+        }
+    }
+}
