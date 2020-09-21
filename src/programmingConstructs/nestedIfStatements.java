3359c2c2bfe30c0016576e65c1a51e13325cf3a5
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2020-09-22 01:42:45 +0530

    Program showing the use of nested If block in a program.

---
+++ src/programmingConstructs/nestedIfStatements.java
@@ -0,0 +1,18 @@
+package programmingConstructs;
+
+public class nestedIfStatements {
+    /*in this program we learn how to use nested if statement in java
+     *
+     * here if first condition evaluates to true then second condition is checked to produce an output.
+     */
+
+    public static void main(String[] args) {
+        int number1 = 14, number2 = 23;
+        if (number1 > 10) {
+            if (number2 > 20) {
+                System.out.println("Number1 is = " + number1 + " and " + "Number2 is = " + number2);
+            }
+        }
+
+    }
+}
