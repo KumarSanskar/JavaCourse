27fda8ea6c6c9a6962a7f873402728e46af55c92
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2020-09-22 01:58:08 +0530

    Program showing how to use command line arguments for printing output to screen.

---
+++ src/printingToConsoleOutput/usingCommandLineArguments.java
@@ -0,0 +1,14 @@
+package printingToConsoleOutput;
+
+public class usingCommandLineArguments {
+    /*
+     *here we show the use of command line arguments in java
+     *
+     *to use command line arguments we click on Run > Edit configurations > Set value in the box of program arguments.
+     */
+
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+    }
+}
