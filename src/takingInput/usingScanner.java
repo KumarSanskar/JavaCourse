1278b7c0da2775834e29967425d67ea769e9807c
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2020-09-22 23:14:22 +0530

    using scanner class to take input in a program and print output.

---
+++ src/takingInput/usingScanner.java
@@ -0,0 +1,16 @@
+package takingInput;
+
+import java.util.Scanner;
+
+public class usingScanner {
+    /*
+     *here we demonstrate how to take input from user by Scanner class and print output.
+     */
+    public static void main(String[] args) {
+        System.out.println("Enter your name: ");
+        Scanner scannerObject = new Scanner(System.in);
+        String inputName = scannerObject.nextLine();
+
+        System.out.println("The name is: " + inputName);
+    }
+}
