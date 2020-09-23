6e8968ecca528dbb5377ef7dbb13fb41c6ba56e0
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2020-09-24 01:55:41 +0530

    using the for loop to print statements.

---
+++ src/programmingConstructs/usingForLoop.java
@@ -0,0 +1,22 @@
+package programmingConstructs;
+
+import java.util.Scanner;
+
+public class usingForLoop {
+    public static int NUMBER_ITERATIONS = 5;
+
+    /*
+     *here we demonstrate the of use the for loop for printing input name five times.
+     */
+    public static void main(String[] args) {
+        System.out.println("Please enter your name: ");
+        Scanner scannerObject = new Scanner(System.in);
+        String studentName = scannerObject.next();
+
+        for (int NUMBER_ITERATORS = 0; NUMBER_ITERATORS < 5; NUMBER_ITERATORS++) {
+            System.out.println("Your name is: " + studentName);
+
+        }
+    }
+}
+
