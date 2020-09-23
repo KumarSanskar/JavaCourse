6c167143b7751850f286fe3dc85e6379a5c0e8ba
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2020-09-24 02:04:05 +0530

    using the for while loop to print squares of numbers from 1 to 100.

---
+++ src/programmingConstructs/usingWhileLoop.java
@@ -0,0 +1,14 @@
+package programmingConstructs;
+
+public class usingWhileLoop {
+    /*
+     *her ewe use while loop to print squares of number from 1 to 100.
+     */
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i + "^2 =" + i * i);
+            i++;
+        }
+    }
+}
